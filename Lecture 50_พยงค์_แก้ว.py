5897bcf565ee6066a7c257ca1d0a4369fb946fdb
Cokeza1230za <131156098+Cokeza1230za@users.noreply.github.com>
2023-04-23 23:05:22 +0700

    Update Lecture 50_พยงค์_แก้ว.py

--- Lecture 50_พยงค์_แก้ว.py
+++ Lecture 50_พยงค์_แก้ว.py
@@ -1,21 +1,20 @@
-def plusNumber(x,y):
+def plusNumber(x, y):
     print(x + y)
 
 
-def MinusNumber(x,y):
-    print(x-y)
+def MinusNumber(x, y):
+    print(x - y)
 
 
-def MlitipliedbyNumber(x,y):
-    print(x*y)
+def MlitipliedbyNumber(x, y):
+    print(x * y)
 
 
-def DividedbyNumber(x,y):
-    print(x/y)
-
-plusNumber(20,5)
-MinusNumber(20,5)
-MlitipliedbyNumber(20,5)
-DividedbyNumber(20,5)
+def DividedbyNumber(x, y):
+    print(x / y)
 
 
+plusNumber(20, 5)
+MinusNumber(20, 5)
+MlitipliedbyNumber(20, 5)
+DividedbyNumber(20, 5)
