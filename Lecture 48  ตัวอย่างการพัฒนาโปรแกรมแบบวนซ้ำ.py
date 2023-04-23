9831b94b80d9a145af1522bf11896bcef76d35ab
Cokeza1230za <131156098+Cokeza1230za@users.noreply.github.com>
2023-04-23 18:27:32 +0700

    Home Work

--- Lecture 48  ตัวอย่างการพัฒนาโปรแกรมแบบวนซ้ำ.py
+++ Lecture 48  ตัวอย่างการพัฒนาโปรแกรมแบบวนซ้ำ.py
@@ -1,4 +1,5 @@
 inputNumbers = int(input("กรอกตัวเลข : "))
 for i in range(inputNumbers):
-    print((" " * (inputNumbers - i - 1)) + ("*" + ("*" * (i * 2))))
+    print((" " * (inputNumbers - (i+1)) + "*"*((i*2)+1)))
+
 
