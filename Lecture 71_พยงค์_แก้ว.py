0f2488e73fa6efb052031c62bee9eb16723338b9
Cokeza1230za <131156098+Cokeza1230za@users.noreply.github.com>
2023-05-05 20:53:50 +0700

    HomeWork การใช้ คอเลคชั่นต่างๆ 01
    Gu Rock \m/

---
+++ Lecture 71_พยงค์_แก้ว.py
@@ -0,0 +1,24 @@
+menuList = []
+priceList = []
+
+
+def showBill():
+    print('Cokeza007'.center(25, '-'))
+    total = 0
+    for number in range(len(menuList)):
+        print(menuList[number], priceList[number])
+        total += int(priceList[number])
+    print("ราคารวมทั้งหมด:", total, "บาท")
+
+while True:
+    menuName = input("ใส่เมนูมาไอน้อง >>")
+    if (menuName.lower() == "exit"):
+        print("ออกเลยไอน้อง")
+        break
+    else:
+        menuPrice = input("ใส่ราคามาไอน้อง >>")
+        menuList.append(menuName)
+        priceList.append(menuPrice)
+
+print(menuList, priceList)
+showBill()
